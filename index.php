e5f099ab723f3768246df97c4bdefe01ac1cd2f5
QuickMash <106212829+QuickMash@users.noreply.github.com>
2024-03-25 11:59:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Homepage</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="source/styles.css">
+<style>
+</style>
+<script src="source/scrips.js"></script>
+<body>
+  <div class="nav">
+    <a class="nav.bar">Home</a>
+    <a class="nav.bar">Contact</a>
+    <a class="nav.bar">About</a>
+    <a class="nav.bar">Other</a>
+  </div>
+  <div class="content.block">
+    <h1 class="content.title">Homepage</h1>
+    <h2 class="content.subtitle"></h2>
+    <p class="content.p"></p>
+  </div>
+</body>
+</html>
